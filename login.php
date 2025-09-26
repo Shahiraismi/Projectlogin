e2402c79d4127266e188d1471654d6ea9ae6e905
Shahiraismi <shahiraismi332@gmail.com>
2025-09-26 11:01:21 +0700

    Dashboard

--- login.php
+++ login.php
@@ -7,6 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
      // Login sederhana (username: admin, password: 123)
      if ($username === 'admin' && $password === '123') {
+        $_SESSION['username'] = $username;
         $_SESSION['role'] = 'Siswa';
         header("Location: dasboard.php");
         exit;
@@ -16,18 +17,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
      }
      }
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>Login</title>
 </head>
 <body>
-    <h2>From Login</h2>
+    <h2>Form Login</h2>
 
-    <form method="post">
-        Username: <input type="text" name="username" required><br><br>
-        Password: <input type="passwort" name="passwort" required><br><br>
+    <Form method="post">
+        username: <input type="text" name="username" required><br><br>
+        password: <input type="text" name="password" required><br><br>
         <button type="submit">Login</button>
 </form>
 </body>
-</html>        
+</html>
