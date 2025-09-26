f43ebdccbeb63b61ad0afeff4653cebdea63220a
Shahiraismi <shahiraismi332@gmail.com>
2025-09-26 10:41:46 +0700

    proses login

--- login.php
+++ login.php
@@ -1,3 +1,21 @@
+<?php
+session_start();
+// proses login saat Form dikirim
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'] ?? '';
+     $password = $_POST['password'] ?? '';
+    
+     // Login sederhana (username: admin, password: 123)
+     if ($username === 'admin' && $password === '123') {
+        $_SESSION['role'] = 'Siswa';
+        header("Location: dasboard.php");
+        exit;
+     } else {
+    $error = "Username atau password salah!";
+
+     }
+     }
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -10,6 +28,6 @@
         Username: <input type="text" name="username" required><br><br>
         Password: <input type="passwort" name="passwort" required><br><br>
         <button type="submit">Login</button>
-</from>
+</form>
 </body>
 </html>        
